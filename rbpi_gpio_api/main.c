3115de3d104af85fc2786674e13f58512cca65eb
Thomas Vogg <thomas.vogg@posteo.de>
2021-04-26 20:38:05 +0100

    Included test of new method.

--- rbpi_gpio_api/main.c
+++ rbpi_gpio_api/main.c
@@ -7,7 +7,10 @@
 #include "gpio_api.h"
 #include "err_exit.h"
 
+#define BUFFER_SIZE 256
+
 int pin = 14;
+char buffer[BUFFER_SIZE];
 
 void sig_handler(int sig) {
     gpio_cleanup(14);
@@ -23,8 +26,12 @@ int main(void) {
 
     while(1) {
         gpio_set_value(pin, VALUE_ON);
+        gpio_get_value(pin, buffer);
+        printf("value when on: %s\n", buffer);
         usleep(50000);
         gpio_set_value(pin, VALUE_OFF);
+        gpio_get_value(pin, buffer);
+        printf("value when off: %s\n", buffer);
         usleep(50000);
     }
 
